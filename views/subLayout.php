e3a56c48414393dc975d315e65db1135c740ad49
Themroc <7hemroc@gmail.com>
2022-02-12 05:11:01 +0100

    Add subLayout

---
+++ views/subLayout.php
@@ -0,0 +1,18 @@
+<?php
+
+$model= $this->context->getModel();
+$widget= $this->context->getWidget();
+
+$this->beginContent('@admin/views/layouts/main.php');
+
+echo '<div class="panel panel-default">'."\n";
+echo '	<div class="panel-heading">'."\n";
+echo '		<strong>'.$model->mod['name'].'</strong> '.Yii::t('ModHelperModule.base', 'module configuration')."\n";
+echo '	</div>'."\n";
+echo $widget::widget();
+echo '	<div class="panel-body">'."\n";
+echo '		' . $content . "\n";
+echo '	</div>'."\n";
+echo '</div>'."\n";
+
+$this->endContent();
